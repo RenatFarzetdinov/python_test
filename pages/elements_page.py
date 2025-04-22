f573db900bead2453ae99801de570d2eccd12122
Farze <farzetdinovr@gmail.com>
2025-04-22 17:53:12 +0300

    1111

---
+++ pages/elements_page.py
@@ -0,0 +1,16 @@
+import time
+
+from locations.elements_page_locators import TextBoxPageLocators
+from pages.base_page import BasePage
+
+
+
+class TextBoxPage(BasePage):
+    locators = TextBoxPageLocators()
+
+    def fill_all_felds(self,):
+        self.element_is_visible(self.locators.FULL_NAME).send_keys('test')
+        self.element_is_visible(self.locators.EMAIL).send_keys(test@test.tt)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('test test')
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('test test test')
+        self.element_is_visible(self.locators.SUBMIT).click()
